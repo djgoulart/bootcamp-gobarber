b72d5165f3e16617554ef3fe05f7d1ee2c08460c
Diego Goulart <diegogoulart.aws@gmail.com>
2020-05-28 21:52:42 -0300

    first commit

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,34 @@
+import { startOfHour } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import Appointment from '../models/Appointment';
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ provider, date }: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    if (this.appointmentsRepository.findByDate(appointmentDate)) {
+      throw Error('This appointment is already booked');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
